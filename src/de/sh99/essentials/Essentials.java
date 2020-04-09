0ce3e8bb5c71ca217baba91e7338432ca68a2965
thelejla <lejla23012017@gmail.com>
2020-04-09 20:40:16 +0200

    out folder added

---
+++ src/de/sh99/essentials/Essentials.java
@@ -0,0 +1,12 @@
+package de.sh99.essentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Essentials extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
